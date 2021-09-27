3756b44e4d33c97121a4e5b77f7b989797c2252e
kibra <62299912+kibradev@users.noreply.github.com>
2021-09-27 20:27:40 +0300

    QBCore

---
+++ QBCore/config.lua
@@ -0,0 +1,18 @@
+Config = {}
+
+
+ 
+Config.Auth = {
+  { name = "kiibra", identifier = "license:80877aca6164d3df0a469fe3c96f2524127ab052", perm = "admin" },
+}
+
+
+
+Config.Perms = {
+
+  ["admin"] = { 'auth', 'revive', 'heal', 'goto', 'bring', 'kill', 'kick', 'ban', 'givecar', 'giveownedcar', 'freeze', 'openinv', 'clearinv', 'giveitem', 'setjob', 'takess', 'wipe', 'weather', 'time', 'playerblips','cleararea', 'announcement','reviveall', 'bringall', 'kickall', 'deleteallcars', 'wipeoffline', 'banoffline', 'tpm', 'fixcar', 'copy', 'noclip', 'vanish', 'playerlist', 'bansshow', 'bansdelete', 'judge' },
+  ["moderaor"]  = { 'auth', 'revive', 'heal', 'goto', 'bring','kick', 'setjob' },
+
+}
+ 
+ 
