488e4c140988bc74657e7585e527ccfcef75e45e
Jagen0 <79335555+Jagen0@users.noreply.github.com>
2021-11-14 15:59:13 +0300

    Code beautify

--- ESX/fxmanifest.lua
+++ ESX/fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 ui_page "html/index.html"
 
 client_scripts {
-    'client.lua',
+	'client.lua',
 	-- UTILS --
 	'utils/entityiter.lua', -- https://gist.github.com/IllidanS4/9865ed17f60576425369fc1da70259b2
 	'utils/Instructional_Buttons.lua', -- https://github.com/AntwanR942/No-clip/
@@ -13,16 +13,14 @@ client_scripts {
 }
 
 server_scripts {
-    'utils/language.lua',
-    'config.lua',
-    'server.lua',
+	'utils/language.lua',
+	'config.lua',
+	'server.lua',
 }
  
-
 files {
-    "script.js",
-    'html/*.html',
-    'html/js/*.js',
-    'html/css/*.css'
+	"script.js",
+	'html/*.html',
+	'html/js/*.js',
+	'html/css/*.css'
 }
-
