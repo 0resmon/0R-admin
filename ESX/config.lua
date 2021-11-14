c401bcbeeda892689ef7170c61940440f43f0a3a
Jagen0 <79335555+Jagen0@users.noreply.github.com>
2021-11-14 15:54:55 +0300

    Code beautify

--- ESX/config.lua
+++ ESX/config.lua
@@ -1,63 +1,62 @@
 Config = {
-    ImageWebhook = "Webhook",
-    InventoryType = "qb-inventory", -- or m3_inventoryhud
-    StatusEsx = true,
-    Auth = {
-        {
-            name = "NAME",
-            identifier = "İDENTIFIER",
-            perm = "PERM"
-        }
-    },
-    Perms = {
-        ["admin"] = {
-            'auth',
-            'revive',
-            'heal',
-            'goto',
-            'bring',
-            'kill',
-            'kick',
-            'ban',
-            'givecar',
-            'giveownedcar',
-            'freeze',
-            'openinv',
-            'clearinv',
-            'giveitem',
-            'setjob',
-            'takess',
-            'wipe',
-            'weather',
-            'time',
-            'playerblips',
-            'cleararea',
-            'announcement',
-            'reviveall',
-            'bringall',
-            'kickall',
-            'deleteallcars',
-            'wipeoffline',
-            'banoffline',
-            'tpm',
-            'fixcar',
-            'copy',
-            'noclip',
-            'vanish',
-            'playerlist',
-            'bansshow',
-            'bansdelete',
-            'judge' 
-        },
-        ["moderaor"] = {
-            'auth',
-            'revive',
-            'heal',
-            'goto',
-            'bring',
-            'kick',
-            'setjob' 
-        }
-    }
+	ImageWebhook = "Webhook",
+	InventoryType = "qb-inventory", -- or m3_inventoryhud
+	StatusEsx = true,
+	Auth = {
+		{
+			name = "NAME",
+			identifier = "İDENTIFIER",
+			perm = "PERM"
+		}
+	},
+	Perms = {
+		["admin"] = {
+			'auth',
+			'revive',
+			'heal',
+			'goto',
+			'bring',
+			'kill',
+			'kick',
+			'ban',
+			'givecar',
+			'giveownedcar',
+			'freeze',
+			'openinv',
+			'clearinv',
+			'giveitem',
+			'setjob',
+			'takess',
+			'wipe',
+			'weather',
+			'time',
+			'playerblips',
+			'cleararea',
+			'announcement',
+			'reviveall',
+			'bringall',
+			'kickall',
+			'deleteallcars',
+			'wipeoffline',
+			'banoffline',
+			'tpm',
+			'fixcar',
+			'copy',
+			'noclip',
+			'vanish',
+			'playerlist',
+			'bansshow',
+			'bansdelete',
+			'judge'
+		},
+		["moderaor"] = {
+			'auth',
+			'revive',
+			'heal',
+			'goto',
+			'bring',
+			'kick',
+			'setjob'
+		}
+	}
 }
- 
